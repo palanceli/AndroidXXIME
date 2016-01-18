982f0be300c4e0e9734c6be476e7e39dc6201347
palanceli <palanceli@163.com>
2016-01-19 00:10:32 +0800

    no message

---
+++ app/src/main/java/com/binglen/androidxxime/CandidateView.java
@@ -0,0 +1,13 @@
+package com.binglen.androidxxime;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by palance on 16/1/19.
+ */
+public class CandidateView extends View {
+    public CandidateView(Context context){
+        super(context);
+    }
+}
