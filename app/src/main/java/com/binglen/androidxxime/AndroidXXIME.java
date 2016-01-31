7e361e74be805f580cfad66089d5ea4e452b81de
Palance <palanceli@163.com>
2016-01-31 22:56:39 +0800

    完成转换功能，添加CandidateView

--- app/src/main/java/com/binglen/androidxxime/AndroidXXIME.java
+++ app/src/main/java/com/binglen/androidxxime/AndroidXXIME.java
@@ -4,9 +4,14 @@ import android.inputmethodservice.InputMethodService;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.media.AudioManager;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
+import android.view.inputmethod.InputMethodManager;
+
+import java.util.ArrayList;
 
 /**
  * Created by palance on 16/1/11.
@@ -18,21 +23,60 @@ public class AndroidXXIME extends InputMethodService
     private Keyboard keyboard;  // 对应qwerty.xml中定义的Keyboard
     private CandidateView candidateView;
 
-    @Override public void onCreate(){
-        int i=0;
-        i++;
+    private InputMethodManager m_inputMethodManager;
 
+    private StringBuilder m_composeString = new StringBuilder();
+
+    @Override public void onCreate(){
+        Log.d(this.getClass().toString(), "onCreate: ");
+        super.onCreate();
+        m_inputMethodManager = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
     }
 
     @Override public void onInitializeInterface(){
-
+        Log.d(this.getClass().toString(), "onInitializeInterface: ");
     }
 
-/*    @Override public View onCreateCandidatesView(){
+    @Override
+    public View onCreateInputView() {
+        Log.d(this.getClass().toString(), "onCreateInputView: ");
+        // keyboard被创建后，将调用onCreateInputView函数
+        keyboardView = (KeyboardView)getLayoutInflater().inflate(R.layout.keyboard, null);  // 此处使用了keyboard.xml
+        keyboard = new Keyboard(this, R.xml.qwerty);  // 此处使用了qwerty.xml
+        keyboardView.setKeyboard(keyboard);
+        keyboardView.setOnKeyboardActionListener(this);
+        return keyboardView;
+    }
+
+    @Override public View onCreateCandidatesView(){
+        Log.d(this.getClass().toString(), "onCreateCandidatesView: ");
         candidateView = new CandidateView(this);
         candidateView.setService(this);
         return candidateView;
-    }*/
+    }
+
+    @Override public void onStartInput(EditorInfo editorInfo, boolean restarting){
+        super.onStartInput(editorInfo, restarting);
+        Log.d(this.getClass().toString(), "onStartInput: ");
+
+        m_composeString.setLength(0);
+        updateCandidates();
+
+    }
+
+    private void updateCandidates(){
+        Log.d(this.getClass().toString(), "updateCandidates: ");
+        if(m_composeString.length() > 0){
+            setCandidatesViewShown(true);
+        }else{
+            setCandidatesViewShown(false);
+        }
+        if(candidateView != null){
+            ArrayList<String> list = new ArrayList<String>();
+            list.add(m_composeString.toString());
+            candidateView.setSuggestions(list, true, true);
+        }
+    }
 
     @Override
     public void onPress(int primaryCode) {
@@ -62,15 +106,7 @@ public class AndroidXXIME extends InputMethodService
     public void swipeUp() {
     }
 
-    @Override
-    public View onCreateInputView() {
-        // keyboard被创建后，将调用onCreateInputView函数
-        keyboardView = (KeyboardView)getLayoutInflater().inflate(R.layout.keyboard, null);  // 此处使用了keyboard.xml
-        keyboard = new Keyboard(this, R.xml.qwerty);  // 此处使用了qwerty.xml
-        keyboardView.setKeyboard(keyboard);
-        keyboardView.setOnKeyboardActionListener(this);
-        return keyboardView;
-    }
+
 
     private void playClick(int keyCode){
         // 点击按键时播放声音，在onKey函数中被调用
@@ -103,7 +139,18 @@ public class AndroidXXIME extends InputMethodService
                 break;
             default:
                 char code = (char)primaryCode;
-                ic.commitText(String.valueOf(code), 1);
+                if(code == ' '){
+                    if(m_composeString.length() > 0) {
+                        ic.commitText(m_composeString, m_composeString.length());
+                        m_composeString.setLength(0);
+                    }else{
+                        ic.commitText(" ", 1);
+                    }
+                }else {
+                    m_composeString.append(code);
+                    ic.setComposingText(m_composeString, 1);
+                }
+                updateCandidates();
         }
     }
 }
--- app/src/main/java/com/binglen/androidxxime/CandidateView.java
+++ app/src/main/java/com/binglen/androidxxime/CandidateView.java
@@ -1,36 +1,207 @@
 package com.binglen.androidxxime;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by palance on 16/1/19.
  */
 public class CandidateView extends View {
-    private AndroidXXIME xxIME;
-    private String m_inputString = new String("");
-    private Paint m_textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
-    public CandidateView(Context context){
+    private static final int OUT_OF_BOUNDS = -1;
+
+    private AndroidXXIME mService;
+    private List<String> mSuggestions;
+//    private int mSelectedIndex;
+    private int mTouchX = OUT_OF_BOUNDS;
+    private Drawable mSelectionHighlight;
+    private boolean mTypedWordValid;
+
+    private Rect mBgPadding;
+
+    private static final int MAX_SUGGESTIONS = 32;
+    private static final int SCROLL_PIXELS = 20;
+
+    private int[] mWordWidth = new int[MAX_SUGGESTIONS];
+    private int[] mWordX = new int[MAX_SUGGESTIONS];
+
+    private static final int X_GAP = 10;
+
+    private static final List<String> EMPTY_LIST = new ArrayList<String>();
+
+    private int mColorNormal;
+    private int mColorRecommended;
+    private int mColorOther;
+    private int mVerticalPadding;
+    private Paint mPaint;
+    private boolean mScrolled;
+    private int mTargetScrollX;
+
+    private int mTotalWidth;
+
+    private GestureDetector mGestureDetector;
+
+    /**
+     * Construct a CandidateView for showing suggested words for completion.
+     * @param context
+     */
+    public CandidateView(Context context) {
         super(context);
+        Log.d(this.getClass().toString(), "CandidateView: ");
+        mSelectionHighlight = context.getResources().getDrawable(
+                android.R.drawable.list_selector_background);
+        mSelectionHighlight.setState(new int[]{
+                android.R.attr.state_enabled,
+                android.R.attr.state_focused,
+                android.R.attr.state_window_focused,
+                android.R.attr.state_pressed
+        });
+
+        Resources r = context.getResources();
+
+        setBackgroundColor(getResources().getColor(R.color.candidate_background, null));
+
+        mColorNormal = r.getColor(R.color.candidate_normal, null);
+        mColorRecommended = r.getColor(R.color.candidate_recommended, null);
+        mColorOther = r.getColor(R.color.candidate_other, null);
+        mVerticalPadding = r.getDimensionPixelSize(R.dimen.candidate_vertical_padding);
+
+        mPaint = new Paint();
+        mPaint.setColor(mColorNormal);
+        mPaint.setAntiAlias(true);
+        mPaint.setTextSize(r.getDimensionPixelSize(R.dimen.candidate_font_height));
+        mPaint.setStrokeWidth(0);
+
+        setHorizontalFadingEdgeEnabled(true);
+        setWillNotDraw(false);
+        setHorizontalScrollBarEnabled(false);
+        setVerticalScrollBarEnabled(false);
     }
 
-    public CandidateView(Context context, AttributeSet set){
-        super(context, set);
-        m_textPaint.setTextSize(100);
-        m_textPaint.setColor(Color.BLACK);
+    /**
+     * A connection back to the service to communicate with the text field
+     * @param listener
+     */
+    public void setService(AndroidXXIME listener) {
+        mService = listener;
     }
 
-    public void setService(AndroidXXIME androidXXIME){
-        xxIME = androidXXIME;
+    @Override
+    public int computeHorizontalScrollRange() {
+        return mTotalWidth;
     }
 
-    @Override public void onDraw(Canvas canvas){
-        super.onDraw(canvas);
-        canvas.drawText(m_inputString, 0, 100, m_textPaint);
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        Log.d(this.getClass().toString(), "onMeasure: ");
+        int measuredWidth = resolveSize(50, widthMeasureSpec);
+
+        // Get the desired height of the icon menu view (last row of items does
+        // not have a divider below)
+        Rect padding = new Rect();
+        mSelectionHighlight.getPadding(padding);
+        final int desiredHeight = ((int)mPaint.getTextSize()) + mVerticalPadding
+                + padding.top + padding.bottom;
+
+        // Maximum possible width and desired height
+        setMeasuredDimension(measuredWidth,
+                resolveSize(desiredHeight, heightMeasureSpec));
+    }
+
+    /**
+     * If the canvas is null, then only touch calculations are performed to pick the target
+     * candidate.
+     */
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Log.d(this.getClass().toString(), "onDraw: ");
+        if (canvas != null) {
+            super.onDraw(canvas);
+        }
+        mTotalWidth = 0;
+        if (mSuggestions == null) return;
+
+        if (mBgPadding == null) {
+            mBgPadding = new Rect(0, 0, 0, 0);
+            if (getBackground() != null) {
+                getBackground().getPadding(mBgPadding);
+            }
+        }
+        int x = 0;
+        final int count = mSuggestions.size();
+        final int height = getHeight();
+        final Rect bgPadding = mBgPadding;
+        final Paint paint = mPaint;
+        final int touchX = mTouchX;
+        final int scrollX = getScrollX();
+        final boolean scrolled = mScrolled;
+        final boolean typedWordValid = mTypedWordValid;
+        final int y = (int) (((height - mPaint.getTextSize()) / 2) - mPaint.ascent());
+
+        for (int i = 0; i < count; i++) {
+            String suggestion = mSuggestions.get(i);
+            float textWidth = paint.measureText(suggestion);
+            final int wordWidth = (int) textWidth + X_GAP * 2;
+
+            mWordX[i] = x;
+            mWordWidth[i] = wordWidth;
+            paint.setColor(mColorNormal);
+            if (touchX + scrollX >= x && touchX + scrollX < x + wordWidth && !scrolled) {
+                if (canvas != null) {
+                    canvas.translate(x, 0);
+                    mSelectionHighlight.setBounds(0, bgPadding.top, wordWidth, height);
+                    mSelectionHighlight.draw(canvas);
+                    canvas.translate(-x, 0);
+                }
+//                mSelectedIndex = i;
+            }
+
+            if (canvas != null) {
+                if ((i == 1 && !typedWordValid) || (i == 0 && typedWordValid)) {
+                    paint.setFakeBoldText(true);
+                    paint.setColor(mColorRecommended);
+                } else if (i != 0) {
+                    paint.setColor(mColorOther);
+                }
+                canvas.drawText(suggestion, x + X_GAP, y, paint);
+                paint.setColor(mColorOther);
+                canvas.drawLine(x + wordWidth + 0.5f, bgPadding.top,
+                        x + wordWidth + 0.5f, height + 1, paint);
+                paint.setFakeBoldText(false);
+            }
+            x += wordWidth;
+        }
+        mTotalWidth = x;
+    }
+
+    public void setSuggestions(List<String> suggestions, boolean completions,
+                               boolean typedWordValid) {
+        clear();
+        if (suggestions != null) {
+            mSuggestions = new ArrayList<String>(suggestions);
+        }
+        mTypedWordValid = typedWordValid;
+        invalidate();
+        requestLayout();
+    }
+
+    public void clear() {
+        mSuggestions = EMPTY_LIST;
+//        mTouchX = OUT_OF_BOUNDS;
+        invalidate();
     }
 }
+
